b902212d7e40b6a4bf488d2def3e82f1baf80d28
Mariusz Wieczorek <m.wieczorek1972@gmail.com>
2020-12-20 23:46:26 +0100

    Add project files.

---
+++ ReportService.cs
@@ -0,0 +1,66 @@
+﻿using ReportService.Repositories;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace ReportService
+{
+    public partial class ReportService : ServiceBase
+    {
+        private const int _sendHour = 8;
+        private const int _intervalInMinutes = 30;
+        private Timer _timer = new Timer(_intervalInMinutes * 1000); // ważne wybieramy namespace: System.Timers 
+        private ErrorRepository _errorRepository = new ErrorRepository();
+
+        /// <summary>
+        /// Metoda, która będzie wywoływana co określony interval
+        /// </summary>
+        public ReportService()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            _timer.Elapsed  += DoWork;
+            _timer.Start();
+        }
+
+        private void DoWork(object sender, ElapsedEventArgs e)
+        {
+            SendError();
+            SendReport();
+        }
+
+        private void SendError()
+        {
+            var errors = _errorRepository.GetLastErrors(_intervalInMinutes);
+            if (errors == null || !errors.Any())
+                return;
+
+            // TODO:Send Mail
+            
+        }
+
+        private void SendReport()
+        {
+            var actualHour = DateTime.Now.Hour;
+
+            if (actualHour < _sendHour)
+                return;
+
+        }
+
+
+        protected override void OnStop()
+        {
+        }
+    }
+}
---
+++ Repositories/ErrorRepository.cs
@@ -0,0 +1,24 @@
+﻿using ReportService.Models.Domains;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReportService.Repositories
+{
+    public class ErrorRepository
+    {
+        public List<Error> GetLastErrors(int intervalInMinutes)
+        {
+            // docelowo pobieranie z bazy
+
+            return new List<Error>
+            {
+                new Error {Message = "błąd testowy 1", Date = DateTime.Now},
+                new Error {Message = "błąd testowy 1", Date = DateTime.Now},
+            };
+
+        }
+    }
+}
